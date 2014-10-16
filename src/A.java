c5234f11f9d7e539d5a136618e737d45e4bb6f40
Andreas Petersen <adp@adpet.dk>
2014-10-16 15:34:23 +0200

    1Version
    Should work but doesn't

--- src/A.java
+++ src/A.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class A {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner console = new Scanner(System.in);
+		int stairs = Integer.parseInt(console.next());
+		int multiplier = Integer.parseInt(console.next());
+		int answer = stairs / 2;
+		boolean run = true;
+		
+		if (stairs < multiplier){
+			answer = -1;
+			run = false;
+		}
+		while((answer % multiplier) != 0 && run){
+			answer++;
+		}
+		console.close();
+		System.out.println(answer);
 	}
-
 }
